ce400ecfa537a4998bc6afa827e34db4f9c4c108
Aljawharah004 <444200750@student.ksu.edu>
2025-03-23 23:50:46 +0300

    homepage + club page partially done

---
+++ clubs.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>KSUHub | النوادي</title>
+		
+		<!-- tab icon -->
+		<link rel="icon" href="img/KSUHub2.png" type="image/x-icon">
+
+		<!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- Custom stylesheet -->
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+
+		<!-- Arabic font -->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@100..900&display=swap" rel="stylesheet">
+
+		<!-- Icons -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+
+    </head>
+	<body>
+		<header id="header" class="transparent-nav">
+			<div class="head-container">
+
+				<div class="navbar-header">
+					<!-- Logo -->
+					<div class="navbar-brand">
+						<a class="logo" href="home.html">
+							<img src="./img/logo-alt.png" alt="logo">
+						</a>
+					</div>
+
+					<!-- Mobile toggle -->
+					<button class="navbar-toggle">
+						<span></span>
+					</button>
+				</div>
+
+				<!-- Navigation -->
+				<nav id="nav">
+					<ul class="main-menu nav navbar-nav navbar-right">
+						<li><a href="home.html">الصفحة الرئيسية</a></li>
+						<li><a href="clubs.html"> النوادي</a></li>
+						<li><a href="student-profile.html">الملف الشخصي</a></li>
+					
+						<li class="logout-item"><a href="logout.php" class="logout-button" style="margin-right: 0;"><i class="fas fa-sign-out-alt"></i> تسجيل الخروج</a></li>
+					</ul>
+				</nav>
+
+			</div>
+		</header>
+
+		<div class="hero-area section">
+			<!-- Backgound Image -->
+			<div class="bg-image bg-parallax overlay" style="background-image:url(./img/home-background.png)"></div>
+
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1 text-center">
+						<h1 style="text-align: center;" class="white-text">صفحة النوادي</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Clubs -->
+		<div id="club" class="section">
+
+			<!-- container -->
+			<div class="container">
+				<div class="row">
+                                    
+					<!-- main club -->
+					<div id="main" class="col-md-9">
+						
+                                            <?php
+                                            include 'db_connection.php';
+
+                                            $search = isset($_GET['search']) ? $_GET['search'] : '';
+                                            $sql = "SELECT * FROM adminuser";
+
+                                            if (!empty($search)) {
+                                                $search = $conn->real_escape_string($search);
+                                                $sql .= " WHERE clubName LIKE '%$search%'";
+                                            }
+                                            
+                                            $college = isset($_GET['college']) ? $_GET['college'] : '';
+                                            if (!empty($college)) {
+                                                $college = $conn->real_escape_string($college);
+                                                if (strpos($sql, 'WHERE') !== false) {
+                                                    $sql .= " AND affiliation LIKE '%$college%'";
+                                                } else {
+                                                    $sql .= " WHERE affiliation LIKE '%$college%'";
+                                                }
+                                            }
+                                            $result = $conn->query($sql);
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            if ($result->num_rows > 0) {
+                                                while ($club = $result->fetch_assoc()) {
+                                                    ?>
+                                                    <div class="col-md-6">
+                                                        <div class="single-club">
+                                                            <div class="club-img">
+                                                                <a href="#" class="clubs-clubimg">
+                                                                    <img src="#" alt="<?= $club['clubName'] ?>">
+                                                                </a>
+                                                            </div>
+                                                            <div class="club-name-container">
+                                                                <a class="club-name" href="#">
+                                                                    <?= $club['clubName'] ?>
+                                                                </a>
+                                                            </div>
+                                                            <div class="club-meta">
+<!--                                                                <span class="club-college"> php $club['affiliation'] php </span>-->
+                                                                <div class="pull-right">
+                                                                    <span class="club-meta-author" style="color: <?= $club['is_open'] ? 'green' : 'red' ?>;">
+                                                                        <p>Unkown</p>
+                                                                            <!--How could "availability" be implemented when we don't have a count for members in each club? nor the membership count restrictions of each club-->
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <?php
+                                                }
+                                            } else {
+                                                echo '<p class="text-center">لا توجد أندية حالياً.</p>';
+                                            }
+                                            ?>
+
+						<div class="row">
+							
+                                                    <!-- pagination -->
+							<div class="col-md-12">
+								<div class="post-pagination">
+									<a href="#" class="pagination-back pull-left"><i class="fas fa-arrow-left"></i> للخلف</a>
+									<ul class="pages">
+										<li class="active">1</li>
+										<li><a href="#">2</a></li>
+										<li><a href="#">3</a></li>
+										<li><a href="#">4</a></li>
+									</ul>
+									<a href="#" class="pagination-next pull-right">للأمام<i class="fas fa-arrow-right"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					<!-- aside club -->
+					<div id="aside" class="col-md-3">
+
+						<!-- search widget -->
+						<div class="widget search-widget">                                                        
+                                                        <form method="GET" action="clubs.php">
+                                                            <input class="input" type="text" name="search" placeholder="ابحث عن نادي..." value="<?= isset($_GET['search']) ? htmlspecialchars($_GET['search']) : '' ?>">
+                                                            <button type="submit"><i class="fa fa-search"></i></button>
+                                                        </form> 
+						</div>
+
+						<!-- category widget -->
+						<div class="widget category-widget">
+							<h3>الكليات</h3>
+							<a class="category" href="#">CCIS <span>9</span></a> <!-- computer science -->
+							<a class="category" href="#">CBA<span>4</span></a> <!--Business-->
+							<a class="category" href="#">CD <span>5</span></a> <!-- midcine-->
+							<a class="category" href="#">CAMS <span>7</span></a> <!-- applied medical sciences-->
+							<a class="category" href="#">CTA <span>3</span></a> <!-- tournism and arcmaeology السياحة-->                                 
+                                                </div>
+					</div>
+				</div>
+			</div>
+		</div>
+                
+		<!-- Footer -->
+		<footer id="footer" class="section">
+			<div class="container">
+				<div class="row">
+					<!-- footer logo -->
+					<div class="col-md-6">
+						<div class="footer-logo">
+							<a class="logo" href="home.html">
+								<img src="./img/logo.png" alt="logo">
+							</a>
+						</div>
+					</div>
+
+					<!-- footer nav -->
+					<div class="col-md-6">
+						<ul class="footer-nav">
+							<li><a href="home.html">الصفحة الرئيسية</a></li>
+							<li><a href="clubs.html"> النوادي</a></li>
+							<li><a href="student-profile.html">الملف الشخصي</a></li>
+							<li><a href="contact.html">تواصل معنا</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<!-- row -->
+				<div id="bottom-footer" class="row">
+
+					<!-- copyright -->
+					<div class="col-md-8 col-md-pull-4">
+						<div class="footer-copyright" style="text-align: right;">
+							<span>KSUHub &copy; | جميع الحقوق محفوظة حتى عام 2025</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+                
+                
+		<!-- preloader -->
+		<div id='preloader'><div class='preloader'></div></div>
+
+
+		<!-- jQuery Plugins -->
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+
+	</body>
+</html>
---
+++ home.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>KSUHub | الصفحة الرئيسية  </title>
+		<link rel="icon" href="img/KSUHub2.png" type="image/x-icon">
+		<!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- Custom stylesheet  --> 
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+		<!-- Arabic font -->
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@100..900&display=swap" rel="stylesheet">
+		<!-- Icons -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    </head>
+	<body>
+            <!--debug-->
+		<header id="header" class="transparent-nav">
+			<div class="head-container">
+				<div class="navbar-header">
+					<!-- Logo -->
+					<div class="navbar-brand">
+						<a class="logo" href="home.html">
+							<img src="./img/logo-alt.png" alt="logo">
+						</a>
+					</div>
+
+					<!-- Mobile toggle -->
+					<button class="navbar-toggle">
+						<span></span>
+					</button>
+				</div>
+
+				<!-- Navigation -->
+				<nav id="nav">
+					<ul class="main-menu nav navbar-nav navbar-right">
+						<li><a href="home.html">الصفحة الرئيسية</a></li>
+						<li><a href="clubs.html"> النوادي</a></li>
+						<li><a href="student-profile.html">الملف الشخصي</a></li>
+					
+						<li class="logout-item"><a href="logout.php" class="logout-button" style="margin-right: 0;"><i class="fas fa-sign-out-alt"></i> تسجيل الخروج</a></li>
+					</ul>
+				</nav>
+
+			</div>
+		</header>
+
+		<!-- Home -->
+		<div id="home" class="hero-area">
+			<!-- Backgound Image -->
+			<div class="bg-image bg-parallax overlay" style="background-image:url(./img/home-background.png)"></div>
+			<div class="home-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-8">
+							<h1 class="white-text">وجهتك للفُرص، الفعاليات، والتَجربة</h1>
+							<p class="lead white-text">استكشف أندية جامعة الملك سعود، واجعل حياتك الجامعية أكثر إشراقًا!</p>
+							<a class="main-button icon-button" href="clubs.html">تعرف على النوادي!</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<!-- Clubs -->
+		<div id="clubs" class="section">
+                        
+			<div class="container">
+
+				<!-- row -->
+				<div class="row">
+					<div class="section-header text-center">
+						<h2>أشهر النوادي</h2>
+						<p class="lead">هنا المنافسة والتحدي!</p>
+					</div>
+				</div>
+				<!-- /Inner-Clubs -->
+				<div id="clubs-wrapper">
+                                    <?php
+                                    include 'db_connection.php';
+
+                                    $sql = "SELECT * FROM adminuser LIMIT 8";
+                                    $result = $conn->query($sql);
+
+                                    if ($result->num_rows > 0) {
+                                        $i = 0;
+                                        echo '<div class="row">';
+
+                                        while($club = $result->fetch_assoc()) {
+                                            if ($i > 0 && $i % 4 == 0) {
+                                                echo '</div><div class="row">';
+                                            }
+                                            ?>
+
+                                            <div class="col-md-3 col-sm-6 col-xs-6">
+                                                <div class="club">
+                                                    <!--Undone-->
+                                                    <a href="#" class="club-img">
+                                                        <img src="#" alt="<?= $club['clubName'] ?> logo">
+                                                        <i class="club-link-icon fa fa-link"></i>
+                                                    </a>
+                                                    <!--Undone-->
+                                                    <a class="club-title" href="#"> 
+                                                        <?= $club['clubName'] ?>
+                                                    </a>
+
+                                                    <!-- <div class="club-details">
+                                                        <span class="club-category"> php $club['college'] php </span>
+                                                    </div>-->
+                                                </div>
+                                            </div>
+
+                                            <?php
+                                            $i++;
+                                        }
+
+                                        echo '</div>';
+                                    } else {
+                                        echo '<p>لا توجد أندية متاحة حالياً.</p>';
+                                    }
+                                    ?>
+				</div>
+
+				<div class="row">
+					<div class="center-btn">
+						<a class="main-button icon-button" href="clubs.php">جميع الأندية</a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+                
+                <!--Events-->
+                <?php
+                include 'db_connection.php';
+
+                // rotate events every 3 days
+                $offset = floor((time() - strtotime('2024-01-01')) / (3 * 86400));
+                $event_sql = "SELECT * FROM event LIMIT 1 OFFSET $offset";
+                $event_result = $conn->query($event_sql);
+
+                if ($event_result->num_rows > 0) {
+                    $event = $event_result->fetch_assoc();
+                    ?>
+
+                    <div id="cta" class="section">
+                        <!-- Background Image -->
+<!--                        <div class="bg-image bg-parallax overlay" style="background-image:url(uploads/ php $event['image'] php )"></div>-->
+
+                        <!-- container -->
+                        <div class="container">
+                            <!-- row -->
+                            <div class="row">
+                                <div class="col-md-6">
+<!--                                    <h2 class="white-text"> php $event['title'] php </h2> shouls be title, but we dont have one yet-->
+                                    <p class="lead white-text"><?= $event['eventDescription'] ?></p>
+                                    <a class="main-button icon-button" href="#">!إذهب لصفحة النادي</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                <?php
+                } else {
+                    echo "<p class='text-center'> ترقبوا جديد سعود!</p>";
+                }
+                ?>
+                <?php
+                include 'db_connection.php';
+
+                // rotate events every 3 days
+                $offset = floor((time() - strtotime('2024-01-01')) / (3 * 86400));
+                $event_sql = "SELECT * FROM event LIMIT 1 OFFSET $offset";
+                $event_result = $conn->query($event_sql);
+
+                if ($event_result->num_rows > 0) {
+                    $event = $event_result->fetch_assoc();
+                    ?>
+
+                    <div id="cta" class="section">
+                        <!-- Background Image -->
+                        <div class="bg-image bg-parallax overlay" style="background-image:url(uploads/<?= $event['image'] ?>)"></div>
+
+                        <!-- container -->
+                        <div class="container">
+                            <!-- row -->
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h2 class="white-text"><?= $event['title'] ?></h2>
+                                    <p class="lead white-text"><?= $event['description'] ?></p>
+                                    <a class="main-button icon-button" href="club-profile-user.php?id=<?= $event['club_id'] ?>">!إذهب لصفحة النادي</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                <?php
+                } else {
+                    ?>
+
+                    <div id="cta" class="section" style="background: linear-gradient(135deg, #276880 , #cce5f3);">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h2 class="white-text">⏳</h2>
+                                    <p class="lead white-text">ترقبوا جديد سعود!</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                <?php
+                }
+                ?>
+
+
+		<div id="why-us" class="section">
+
+			<!-- container -->
+			<div class="container">
+
+				<div class="section-header text-center">
+					<h2>من نحن؟</h2>
+					<p class="lead">تعرف على موقعنا!</p>
+				</div>
+
+				<!-- Team Section -->
+				<section id="ksuhub-team" class="ksuhub-section">
+					<div class="ksuhub-container">
+						<div class="ksuhub-header text-center">
+							<h2 class="section-header text-center">فـــريـقنا</h2>
+						</div>
+						<div class="ksuhub-team-grid">
+							<!-- Team Member 1-->
+							<div class="ksuhub-team-card">
+								<div class="ksuhub-team-image">
+									<img src="img/arwa.jpg" alt="Instructor 1" class="ksuhub-img">
+								</div>
+								<div class="ksuhub-team-social">
+									<a href="https://x.com/ar_aam5" class="ksuhub-social-link"><i class="fab fa-twitter"></i></a>
+									<a href="https://www.linkedin.com/in/arwa-a-79b8962a7/" class="ksuhub-social-link"><i class="fab fa-linkedin-in"></i></a>
+								</div>
+								<div class="ksuhub-team-info">
+									<h5 class="ksuhub-instructor-name">أروى المــطيري</h5>
+									<p class="ksuhub-instructor-role">تقنـية المعـلومات</p>
+								</div>
+							</div>
+
+							<!-- Team Member 2-->
+							<div class="ksuhub-team-card">
+								<div class="ksuhub-team-image">
+									<img src="img/jory.jpg" alt="Instructor 2" class="ksuhub-img">
+								</div>
+								<div class="ksuhub-team-social">
+									<a href="https://x.com/iijawharah" class="ksuhub-social-link"><i class="fab fa-twitter"></i></a>
+									<a href="https://www.linkedin.com/in/aljawharah-alwabel-9426a3244/" class="ksuhub-social-link"><i class="fab fa-linkedin-in"></i></a>
+								</div>
+								<div class="ksuhub-team-info">
+									<h5 class="ksuhub-instructor-name">الجــوهرة الوابــل</h5>
+									<p class="ksuhub-instructor-role">تقنـية المعـلومات</p>
+								</div>
+							</div>
+
+							<!-- Team Member 3-->
+							<div class="ksuhub-team-card">
+								<div class="ksuhub-team-image">
+									<img src="img/layan.jpg" alt="Instructor 3" class="ksuhub-img">
+								</div>
+								<div class="ksuhub-team-social">
+									<a href="https://x.com/vxwz0?s=21" class="ksuhub-social-link"><i class="fab fa-twitter"></i></a>
+									<a href="https://www.linkedin.com/in/layan-alfawzan-217178295/" class="ksuhub-social-link"><i class="fab fa-linkedin-in"></i></a>
+								</div>
+								<div class="ksuhub-team-info">
+									<h5 class="ksuhub-instructor-name">لـيـان الفـوزان</h5>
+									<p class="ksuhub-instructor-role">تقنـية المعـلومات</p>
+								</div>
+							</div>
+
+							<!-- Team Member 4-->
+							<div class="ksuhub-team-card">
+								<div class="ksuhub-team-image">
+									<img src="img/rozie.jpg" alt="Instructor 4" class="ksuhub-img">
+								</div>
+								<div class="ksuhub-team-social">
+									<a href="https://x.com/_lr73" class="ksuhub-social-link"><i class="fab fa-twitter"></i></a>
+									<a href="https://www.linkedin.com/in/rose-mady-879895292/" class="ksuhub-social-link"><i class="fab fa-linkedin-in"></i></a>
+								</div>
+								<div class="ksuhub-team-info">
+									<h5 class="ksuhub-instructor-name">روز مــاضي</h5>
+									<p class="ksuhub-instructor-role">تقنـية المعـلومات</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<hr class="section-hr">
+				<div class="row">
+					<div class="col-md-5 col-md-offset-1"> 
+						<a class="about-ksu" href="#">
+							<img src="img/aboutUs.webp" alt="">
+						</a>
+					</div>
+					<div class="col-md-6">
+						<p class="lead">هدفنا منصة موحدة لجميع نوادي جامعة الملك سعود</p>
+						<p>موقعنا هو منصتك لاكتشاف أندية جامعة الملك سعود والانضمام إليها والتفاعل مع أنشطتها. نوفر لك طريقة سهلة للتسجيل في الأندية، متابعة الفعاليات، وتوثيق ساعات التطوع. هدفنا هو تسهيل تجربة المشاركة الطلابية وجعل الحياة الجامعية أكثر إثراءً وتنوعًا.</p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+		<!-- Footer -->
+		<footer id="footer" class="section">
+			<div class="container">
+				<div class="row">
+					<!-- footer logo -->
+					<div class="col-md-6">
+						<div class="footer-logo">
+							<a class="logo" href="home.html">
+								<img src="./img/logo.png" alt="logo">
+							</a>
+						</div>
+					</div>
+
+					<!-- footer nav -->
+					<div class="col-md-6">
+						<ul class="footer-nav">
+							<li><a href="home.html">الصفحة الرئيسية</a></li>
+							<li><a href="clubs.html"> النوادي</a></li>
+							<li><a href="student-profile.html">الملف الشخصي</a></li>
+							<li><a href="contact.html">تواصل معنا</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<!-- row -->
+				<div id="bottom-footer" class="row">
+
+					<!-- copyright -->
+					<div class="col-md-8 col-md-pull-4">
+						<div class="footer-copyright" style="text-align: right;">
+							<span>KSUHub &copy; | جميع الحقوق محفوظة حتى عام 2025</span>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</footer>
+
+		<!-- preloader -->
+		<div id='preloader'><div class='preloader'></div></div>
+
+
+		<!-- jQuery Plugins -->
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+
+	</body>
+</html>
